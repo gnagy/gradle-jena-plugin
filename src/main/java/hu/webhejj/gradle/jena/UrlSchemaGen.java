d2740fec4da5dec9b2af6b134c2b011cc19e9ab4
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2016-06-17 14:52:13 +0200

    url schemagen

---
+++ src/main/java/hu/webhejj/gradle/jena/UrlSchemaGen.java
@@ -0,0 +1,208 @@
+/*
+ *  Copyright Gergely Nagy <greg@webhejj.hu>
+ *
+ *  Licensed under the Apache License, Version 2.0;
+ *  you may obtain a copy of the License at:
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ */
+package hu.webhejj.gradle.jena;
+
+import jena.schemagen;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
+import org.gradle.api.tasks.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UrlSchemaGen extends DefaultTask {
+
+    private static final Logger logger = LoggerFactory.getLogger(UrlSchemaGen.class);
+
+    private URI input;
+    private File outputDirectory;
+    private String packageName;
+    private String classNameSuffix;
+    private boolean inference;
+    private boolean ontology;
+    private boolean nostrict;
+    private boolean nocomments = false;
+    private String namespace;
+
+    @Input
+    public URI getInput() {
+        return input;
+    }
+
+    public void setInput(URI input) {
+        this.input = input;
+    }
+
+    public void input(URI input) {
+        setInput(input);
+    }
+
+    @OutputDirectory
+    public File getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    public void setOutputDirectory(File outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    public void outputDirectory(File outputDirectory) {
+        setOutputDirectory(outputDirectory);
+    }
+
+    @Input
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public void packageName(String packageName) {
+        setPackageName(packageName);
+    }
+
+    @Input
+    @Optional
+    public String getClassNameSuffix() {
+        return classNameSuffix;
+    }
+
+    public void setClassNameSuffix(String classNameSuffix) {
+        this.classNameSuffix = classNameSuffix;
+    }
+
+    public void classNameSuffix(String classNameSuffix) {
+        setClassNameSuffix(classNameSuffix);
+    }
+
+    @Input
+    public boolean isInference() {
+        return inference;
+    }
+
+    public void setInference(boolean inference) {
+        this.inference = inference;
+    }
+
+    public void inference(boolean inference) {
+        setInference(inference);
+    }
+
+    @Input
+    public boolean isOntology() {
+        return ontology;
+    }
+
+    public void setOntology(boolean ontology) {
+        this.ontology = ontology;
+    }
+
+    public void ontology(boolean ontology) {
+        setOntology(ontology);
+    }
+
+    @Input
+    public boolean isNostrict() {
+        return nostrict;
+    }
+
+    public void setNostrict(boolean nostrict) {
+        this.nostrict = nostrict;
+    }
+
+    public void nostrict(boolean nostrict) {
+        setNostrict(nostrict);
+    }
+
+    @Input
+    @Optional
+    public boolean isNocomments() {
+        return nocomments;
+    }
+
+    public void setNocomments(boolean nocomments) {
+        this.nocomments = nocomments;
+    }
+
+    public void nocomments(boolean nocomments) {
+        setNocomments(nocomments);
+    }
+
+    @Input
+    @Optional
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    public void namespace(String namespace) {
+        setNamespace(namespace);
+    }
+
+
+    @TaskAction
+    public void exec() {
+        List<String> options = new ArrayList<String>();
+        options.clear();
+
+        options.add("-i");
+        options.add(getInput().toString());
+
+        options.add("-o");
+        options.add(getOutputDirectory().getAbsolutePath());
+
+        options.add("--package");
+        options.add(getPackageName());
+
+        if (getClassNameSuffix() != null) {
+            options.add("--classnamesuffix");
+            options.add(getClassNameSuffix());
+        }
+        if (isInference()) {
+            options.add("--inference");
+        }
+        if (isOntology()) {
+            options.add("--ontology");
+        }
+        if (isNostrict()) {
+            options.add("--nostrict");
+        }
+        if (isNocomments()) {
+            options.add("--nocomments");
+        }
+        if (getNamespace() != null) {
+            options.add("-a");
+            options.add(getNamespace());
+        }
+
+
+        logger.info("Excecuting schemagen: " + options);
+
+        // only go() will throw exception but it's protected
+        try {
+            new schemagen() {
+                @Override
+                protected void go(SchemagenOptions options) {
+                    super.go(options);
+                }
+            }.go(new schemagen.SchemagenOptionsImpl(options.toArray(new String[options.size()])));
+        } catch (schemagen.SchemagenException e) {
+            throw new GradleException("Error while generating schema", e);
+        }
+    }
+}
